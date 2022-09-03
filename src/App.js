804300fe8465077a83ab02b73c4b11884202b02c
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2022-09-03 19:39:11 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import userEvent from "@testing-library/user-event";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("i run only once.");
+  };
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []);
   return (
     <div>
-      <h1>돌아온걸 환영해!</h1>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
