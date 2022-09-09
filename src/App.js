38c4c2b834dcf6e9e772a4ad47dad2d539070cb3
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2022-09-09 21:26:35 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
-  const iRunOnlyOnce = () => {
-    console.log("i run only once.");
-  };
+  const onChange = (event) => setKeyword(event.target.value);
+
   useEffect(() => {
-    console.log("CALL THE API....");
-  }, []);
+    console.log("I run only once.");
+  }, []); //[]가 비어 있기 때문에 리액트가 보고 있는 것이 없어서 한번만 실행됨
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]); //[]안의 keyword가 변화 될때마다 실행됨
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' & 'counter' changes");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
